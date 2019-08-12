7a121b9c10592aa9b1cb2e09060461df52dafb79
Joe Heck <heckj@mac.com>
2019-08-12 07:03:11 -0700

    publisher tests

--- UsingCombineTests/DeferredPublisherTests.swift
+++ UsingCombineTests/DeferredPublisherTests.swift
@@ -11,4 +11,33 @@ import Combine
 
 class DeferredPublisherTests: XCTestCase {
 
+
+    func testDeferredPublisher() {
+        let expectation = XCTestExpectation(description: self.debugDescription)
+
+        let deferredPublisher = Deferred {
+            return Just("hello")
+            }.eraseToAnyPublisher()
+
+        let cancellable = deferredPublisher
+            .sink(receiveCompletion: { completion in
+                print(".sink() received the completion", String(describing: completion))
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let anError):
+                    XCTFail("No failure should be received from empty")
+                    print("received error: ", anError)
+                    break
+                }
+                expectation.fulfill()
+            }, receiveValue: { valueReceived in
+                XCTAssertEqual(valueReceived, "hello")
+                print(".sink() data received \(valueReceived)")
+            })
+
+        wait(for: [expectation], timeout: 1.0)
+        XCTAssertNotNil(cancellable)
+
+    }
 }
